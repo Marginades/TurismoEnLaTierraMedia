66e7e77321524315e02cb4837d82d117f6da44f3
dieee-93 <dieee.93@gmail.com>
2021-09-09 00:43:16 -0300

    Se agrego la carga de Promociones al administrador de archivos y se cargo promociones.txt

--- src/parqueDeDiversiones/AdministradorDeArchivos.java
+++ src/parqueDeDiversiones/AdministradorDeArchivos.java
@@ -7,97 +7,169 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class AdministradorDeArchivos {
-	
+
 	private List<Atraccion> atracciones = new LinkedList<Atraccion>();
+	private List<Promocion> promociones = new LinkedList<Promocion>();
 	private List<Usuario> usuarios = new LinkedList<Usuario>();
-	
-public List<Atraccion> cargarAtracciones() {
-		
+
+	public List<Atraccion> cargarAtracciones() {
+
 		FileReader fr = null;
-		BufferedReader br = null;	
-	
-	try
-	{
-		
-		
-		fr = new FileReader("atracciones.txt");
-		br = new BufferedReader(fr);
-	
-		
-		String linea;
-		while ((linea = br.readLine()) != null) {
-			String[] propiedadesDeLaAtraccion = linea.split("-");
-			String nombre = propiedadesDeLaAtraccion[0];
-			int costo = Integer.parseInt(propiedadesDeLaAtraccion[1]);
-			double duracion = Double.parseDouble(propiedadesDeLaAtraccion[2]);
-			int cupo = Integer.parseInt(propiedadesDeLaAtraccion[3]);
-			TipoDeAtraccion tipo = TipoDeAtraccion.valueOf(propiedadesDeLaAtraccion[4]);
-
-			
-			atracciones.add(new Atraccion(nombre, costo, duracion, cupo, tipo));
-		
-		
-				
-		}
-
-	}catch(IOException e) {
-		e.printStackTrace();
-	} finally {
+		BufferedReader br = null;
 		try {
-			if (fr != null) {
-				
-				fr.close();
-				
+
+			fr = new FileReader("atracciones.txt");
+			br = new BufferedReader(fr);
+
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				String[] propiedadesDeLaAtraccion = linea.split("-");
+				String nombreAtraccion = propiedadesDeLaAtraccion[0];
+				int costoAtraccion = Integer.parseInt(propiedadesDeLaAtraccion[1]);
+				double duracionAtraccion = Double.parseDouble(propiedadesDeLaAtraccion[2]);
+				int cupoAtraccion = Integer.parseInt(propiedadesDeLaAtraccion[3]);
+				TipoDeAtraccion tipoAtraccion = TipoDeAtraccion.valueOf(propiedadesDeLaAtraccion[4]);
+
+				atracciones.add(new Atraccion(nombreAtraccion, costoAtraccion, duracionAtraccion, cupoAtraccion,
+						tipoAtraccion));
+
 			}
-		} catch (IOException e2) {
-			e2.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+
+					fr.close();
+
+				}
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
 		}
-	}
-	return atracciones;
-}
-
-
-public List<Usuario> cargarUsuarios() {
-	
-	FileReader fr = null;
-	BufferedReader br = null;	
-
-try
-{
-	
-	
-	fr = new FileReader("usuarios.txt");
-	br = new BufferedReader(fr);
-
-	
-	String linea;
-	while ((linea = br.readLine()) != null) {
-		String[] propiedadesDelUsuario = linea.split("-");
-		String nombre = propiedadesDelUsuario[0];
-		int presupuesto = Integer.parseInt(propiedadesDelUsuario[1]);
-		double disponibilidad = Double.parseDouble(propiedadesDelUsuario[2]);
-		TipoDeAtraccion preferencia = TipoDeAtraccion.valueOf(propiedadesDelUsuario[3]);
-
-		
-		usuarios.add(new Usuario(nombre, presupuesto, disponibilidad, preferencia));
-	
-	
-			
+		return atracciones;
 	}
 
-}catch(IOException e) {
-	e.printStackTrace();
-} finally {
-	try {
-		if (fr != null) {
-			
-			fr.close();
-			
+	public List<Promocion> cargarPromociones() {
+
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+
+			fr = new FileReader("Promociones.txt");
+			br = new BufferedReader(fr);
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				String[] propiedadesDeLaPromocion = linea.split("-");
+
+				if (propiedadesDeLaPromocion[0] == "AXB") {
+					String[] listaDeAtracciones = propiedadesDeLaPromocion[3].split("/");
+					LinkedList<Atraccion> atraccionesDeLaPromo = new LinkedList<Atraccion>();
+					TipoDeAtraccion tipoPromocion = TipoDeAtraccion.valueOf(propiedadesDeLaPromocion[1]);
+					String nombrePromocion = propiedadesDeLaPromocion[2];
+
+					for (int i = 0; i < listaDeAtracciones.length; i++) {
+						for (Atraccion c : atracciones) {
+							if (listaDeAtracciones[i] == c.getNombre()) {
+								atraccionesDeLaPromo.add(c);
+							}
+						}
+					}
+					promociones.add(new PromocionAXB(tipoPromocion, nombrePromocion, atraccionesDeLaPromo,
+							atraccionesDeLaPromo.getLast()));
+					atraccionesDeLaPromo = null;
+				}
+
+				if (propiedadesDeLaPromocion[0] == "PORCENTUAL") {
+					String[] listaDeAtracciones = propiedadesDeLaPromocion[4].split("/");
+					LinkedList<Atraccion> atraccionesDeLaPromo = new LinkedList<Atraccion>();
+					TipoDeAtraccion tipoPromocion = TipoDeAtraccion.valueOf(propiedadesDeLaPromocion[1]);
+					String nombrePromocion = propiedadesDeLaPromocion[2];
+					Double descuentoPorcentual = Double.parseDouble(propiedadesDeLaPromocion[3]);
+					for (int i = 0; i < listaDeAtracciones.length; i++) {
+						for (Atraccion c : atracciones) {
+							if (listaDeAtracciones[i] == c.getNombre()) {
+								atraccionesDeLaPromo.add(c);
+							}
+						}
+					}
+					promociones.add(new PromocionPorcentual(tipoPromocion, nombrePromocion, descuentoPorcentual,
+							atraccionesDeLaPromo));
+					atraccionesDeLaPromo = null;
+				}
+				if (propiedadesDeLaPromocion[0] == "ABSOLUTA") {
+					String[] listaDeAtracciones = propiedadesDeLaPromocion[4].split("/");
+					LinkedList<Atraccion> atraccionesDeLaPromo = new LinkedList<Atraccion>();
+					TipoDeAtraccion tipoPromocion = TipoDeAtraccion.valueOf(propiedadesDeLaPromocion[1]);
+					String nombrePromocion = propiedadesDeLaPromocion[2];
+					int descuentoAbsoluto = Integer.parseInt(propiedadesDeLaPromocion[3]);
+					for (int i = 0; i < listaDeAtracciones.length; i++) {
+						for (Atraccion c : atracciones) {
+							if (listaDeAtracciones[i] == c.getNombre()) {
+								atraccionesDeLaPromo.add(c);
+							}
+						}
+					}
+					promociones.add(new PromocionAbsoluta(tipoPromocion, nombrePromocion, descuentoAbsoluto,
+							atraccionesDeLaPromo));
+					atraccionesDeLaPromo = null;
+				}
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+
+					fr.close();
+
+				}
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
 		}
-	} catch (IOException e2) {
-		e2.printStackTrace();
+		return promociones;
 	}
-}
-return usuarios;
-}
+
+
+	public List<Usuario> cargarUsuarios() {
+
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+
+			fr = new FileReader("usuarios.txt");
+			br = new BufferedReader(fr);
+
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				String[] propiedadesDelUsuario = linea.split("-");
+				String nombre = propiedadesDelUsuario[0];
+				int presupuesto = Integer.parseInt(propiedadesDelUsuario[1]);
+				double disponibilidad = Double.parseDouble(propiedadesDelUsuario[2]);
+				TipoDeAtraccion preferencia = TipoDeAtraccion.valueOf(propiedadesDelUsuario[3]);
+
+				usuarios.add(new Usuario(nombre, presupuesto, disponibilidad, preferencia));
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+
+					fr.close();
+
+				}
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+		return usuarios;
+	}
 }
